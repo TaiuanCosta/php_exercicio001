092e34f9dd2d1154ac8801aa0d7d9b05f80c5f15
TaiuanCosta <158339342+TaiuanCosta@users.noreply.github.com>
2025-02-20 17:00:20 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        $nome = "Taiuan";
+        $idade = 19; 
+        $data = date("d/m/Y");
+        $hora = date("H:i:s");
+        $numero1 = 89;
+        $numero2 = 11;
+        $soma = $numero1 + $numero;
+
+        
+
+    echo "<h1>Meu nome é $nome "." e tenho $idade </h1>";
+    echo "Data atual: ". $data . "\n"; 
+    echo "Hora atual: ". $hora. "\n";
+    ?>
+</body>
+</html>
